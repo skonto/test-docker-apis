a4cf54bc238bb7c0e7dd9ad047d3b2534dcd1881
Stavros Kontopoulos <stavros.kontopoulos@lightbend.com>
2020-04-03 12:34:05 +0300

    time fixes

--- skopeoinspect/inspect_test.go
+++ skopeoinspect/inspect_test.go
@@ -17,28 +17,27 @@ func Test_inspect(t *testing.T) {
 
 	ins.Image.DockerImageOptions = DockerImageOptions{Global: &GlobalOptions{}, Shared: &SharedImageOptions{}}
 
-	elapsed := time.Since(time.Now())
+	start := time.Now()
 	err := ins.Run([]string{"docker://docker.io/lightbend/spark-aggregation:134-d0ec286-dirty"}, os.Stdout)
-	fmt.Printf("dockerhub inspect took %s\n", elapsed)
+	fmt.Printf("dockerhub inspect took %s\n", time.Since(start))
 	if err != nil {
 		println(err.Error())
 	}
-	elapsed = time.Since(time.Now())
+	start = time.Now()
 	err = ins.Run([]string{"docker://eu.gcr.io/bubbly-observer-178213/spark-aggregation:134-d0ec286-dirty"}, os.Stdout)
-	fmt.Printf("GCloud inspect took %s\n", elapsed)
+	fmt.Printf("GCloud inspect took %s\n", time.Since(start))
 	if err != nil {
 		println(err.Error())
 	}
-	elapsed = time.Since(time.Now())
+	start = time.Now()
 	err = ins.Run([]string{"docker://405074236871.dkr.ecr.eu-west-1.amazonaws.com/stavros-test/sensor-data-scala:90-d662d87-dirty"}, os.Stdout)
-	fmt.Printf("AWS inspect took %s\n", elapsed)
+	fmt.Printf("AWS inspect took %s\n", time.Since(start))
 	if err != nil {
 		println(err.Error())
 	}
-
-	elapsed = time.Since(time.Now())
+	start = time.Now()
 	err = ins.Run([]string{"docker-daemon:lightbend/spark-aggregation:134-d0ec286-dirty"}, os.Stdout)
-	fmt.Printf("local daemon took %s\n", elapsed)
+	fmt.Printf("local daemon took %s\n", time.Since(start))
 	if err != nil {
 		println(err.Error())
 	}
